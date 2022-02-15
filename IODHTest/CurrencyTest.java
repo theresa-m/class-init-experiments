56378f62d226db81414acdd95aa5a44ec8519583
Theresa Mammarella <mammarellatheresa8@gmail.com>
2022-02-15 09:56:16 -0500

    add currency.data for system property

--- IODHTest/CurrencyTest.java
+++ IODHTest/CurrencyTest.java
@@ -3,8 +3,9 @@ import java.util.Currency;
 public class CurrencyTest {
     /* test behavior when setting system currency through system property java.util.currency.data */
 
+	// -Djava.util.currency.data=currency.data
     static void currencyTest() {
-        Currency c = Currency.getInstance("CA");
-        System.out.println(c.toString());
+        Currency c = Currency.getInstance("CMD");
+        System.out.println(c.toString() + " " + c.getNumericCode());
     }
 }

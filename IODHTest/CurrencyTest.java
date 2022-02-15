c7007965fcbd7d98928f9f7a3f45eb20cc78bc46
Theresa Mammarella <tmammare@redhat.com>
2022-02-15 09:39:10 -0500

    Canada currency code

--- IODHTest/CurrencyTest.java
+++ IODHTest/CurrencyTest.java
@@ -4,7 +4,7 @@ public class CurrencyTest {
     /* test behavior when setting system currency through system property java.util.currency.data */
 
     static void currencyTest() {
-        Currency c = Currency.getInstance("CP");
+        Currency c = Currency.getInstance("CA");
         System.out.println(c.toString());
     }
 }
